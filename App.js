1ae2e3353be211717f78e5565e394e1c0443e2f5
NicolasBQ <nicolasbquintero@gmail.com>
2023-07-18 22:53:14 -0500

    Dimelo yisus

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+import * as Linking from 'expo-linking';
+import * as FileSystem from 'expo-file-system';
+
+import WebView from 'react-native-webview';
+import { shareAsync } from 'expo-sharing';
+
+
+
+if(__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+}
 
 export default function App() {
+  
+  const url = 'https://tallersmart.com:4850/fileview/seefile'
+
+  const myHtml = `
+      <html><body>
+        <h1>SAD LIFE</h1>
+        <iframe
+          width="500" height="500" src="${url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
+        >
+        </iframe>
+      </body></html>
+  `
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView source={{ html: myHtml }} style={{ flex: 1 }} />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'red'
   },
 });
+
+
+
+// COSAS QUE HE PROBADO
+// 1. WebView -> Descarga el archivo, no previsualiza
+// 2. PDFREader -> Esa mondawe no sirve
+// 3. Adobe API -> Me pide dominio de la app (¿? xd) --> Posible volver a revisar
+// 4. Expo FilseSystem y Expo Linking -> No existe un built in scheme para abrir pdfs - https://github.com/expo/expo/issues/3193
+// 5. Intents android actions  -> Esa mondawe no sirve
+// 6. Abrir menu para que el usuario seleccione la app con la que quiere abrir el archivo -> Bare React Native
+
+// Si nada de esto funciona -> Abrir archivo con expo linking a la web
---
+++ ReactotronConfig.js
@@ -0,0 +1,8 @@
+import reactotron from "reactotron-react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+reactotron
+  .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+  .configure() // controls connection & communication settings
+  .useReactNative() // add all built-in react native plugins
+  .connect() // let's connect!
